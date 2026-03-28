939268506dd7bd8a30c7cbc83da326f763ba5b1c
gabentho <gabriel.enthoven@yahoo.fr>
2026-03-28 10:35:49 +0100

    Step3

--- macros/clear_km_relay.sql
+++ macros/clear_km_relay.sql
@@ -1,7 +1,3 @@
-{# TODO: Create a macro that extracts the km value from relay format (e.g., '4x6' → '6')
-   Hint: Use RIGHT(), LENGTH(), and POSITION('x' IN ...) to extract the part after 'x'
-   Don't forget to add an AS alias! #}
-
 {% macro clear_km_relays(column_name) %}
-    {{ column_name }}
+    RIGHT({{ column_name }}, LENGTH({{ column_name }}) - POSITION('x' IN {{ column_name }}))::FLOAT AS km
 {% endmacro %}
--- macros/select_spares.sql
+++ macros/select_spares.sql
@@ -1,8 +1,4 @@
-{# TODO: Create a macro that splits a 'prone+standing' shootings column into two columns
-   Example: '0+1' → shootings = '0', shootings_spare = '1'
-   Hint: Use LEFT(), RIGHT(), LENGTH(), and POSITION('+' IN ...)
-   The macro should output TWO columns separated by a comma #}
-
 {% macro select_spares(column_name) %}
-    {{ column_name }}
+    LEFT({{ column_name }}, POSITION('+' IN {{ column_name }}) - 1)::INT AS shootings_prone,
+    RIGHT({{ column_name }}, LENGTH({{ column_name }}) - POSITION('+' IN {{ column_name }}))::INT AS shootings_standing
 {% endmacro %}
